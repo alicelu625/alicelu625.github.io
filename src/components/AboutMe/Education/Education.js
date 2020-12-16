1df093b00d625fd5679c18ab347bc7c2ad20afea
Alice Lu <alicelu625@gmail.com>
2020-12-15 23:14:21 -0800

    created education component

--- src/components/AboutMe/Education/Education.js
+++ src/components/AboutMe/Education/Education.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import classes from './Education.module.css';
+import EducationItem from './EducationItem/EducationItem';
 
 const education = () => {
     return (
-        <div>
+        <div className={classes.Education}>
             <h2 className={classes.Title}>Education</h2>
+            <EducationItem/>
         </div>
     )
 }
---
+++ src/components/AboutMe/Education/EducationItem/EducationItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './EducationItem.module.css';
+
+const educationItem = () => (
+    <div className={classes.EducationItem}>
+
+    </div>
+);
+
+export default educationItem;
