f62f8fa52d7da8999906b56f35fecb06e4c84642
Alice Lu <alicelu625@gmail.com>
2020-12-28 14:58:10 -0800

    added name to project cover

--- src/components/ProjectItems/ProjectItem/ProjectItem.js
+++ src/components/ProjectItems/ProjectItem/ProjectItem.js
@@ -6,6 +6,9 @@ const projectItem = (props) => {
     return(
         <div className={classes.ProjectItem} onClick={() => props.projectClicked(props.id)}>
             <img className={classes.Cover} src={props.cover} alt={props.altText}/>
+            <div className={classes.NameDiv}>
+                <p className={classes.Name}>{props.name}</p>
+            </div>
         </div>
     );
 }
