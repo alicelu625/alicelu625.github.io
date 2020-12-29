f021b690b3e1e0572b283b2eebd5a94376037926
Alice Lu <alicelu625@gmail.com>
2020-12-28 16:12:44 -0800

    implemented hide top nav on scroll

--- src/components/Navigation/TopNavigation/TopNavigation.js
+++ src/components/Navigation/TopNavigation/TopNavigation.js
@@ -4,16 +4,28 @@ import {FiMenu} from 'react-icons/fi';
 import classes from './TopNavigation.module.css';
 import WebsiteLogo from '../../../assets/images/website-logo.png';
 
-const topNavigation = (props) => (
-    <header>
-        <div className={classes.TopNavigation}>
-            <a href="/">
-                <img className={classes.WebsiteLogo} src={WebsiteLogo} alt="Alice's website logo"/>
-            </a>
-            <FiMenu className={classes.MenuIcon} onClick={props.drawerToggled}/>
-        </div>
-        
-    </header>
-);
+const topNavigation = (props) => {
+    let prevScrollPos = window.pageYOffset;
+    window.onscroll = () => {
+        let currentScrollPos = window.pageYOffset;
+        if (prevScrollPos > currentScrollPos) {
+            document.getElementById("topnav").style.top = "0";
+        } else {
+            document.getElementById("topnav").style.top = "-72px";
+        }
+        prevScrollPos = currentScrollPos;
+    }
+
+    return (
+        <header>
+            <div id="topnav" className={classes.TopNavigation}>
+                <a href="/">
+                    <img className={classes.WebsiteLogo} src={WebsiteLogo} alt="Alice's website logo"/>
+                </a>
+                <FiMenu className={classes.MenuIcon} onClick={props.drawerToggled}/>
+            </div>
+        </header>
+    );
+}
 
 export default topNavigation;
