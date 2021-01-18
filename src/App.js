5709419d0b0712aab69225bd44b07c9184196954
Alice Lu <alicelu625@gmail.com>
2021-01-18 15:45:08 -0800

    set up Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import React, {Component} from 'react';
+import React, {useEffect} from 'react';
 import {Route, Switch} from 'react-router-dom';
+import ReactGa from 'react-ga';
 
 import Layout from './hoc/Layout/Layout';
 import Home from './containers/Home/Home';
 import AboutMe from './containers/AboutMe/AboutMe';
 import Projects from './containers/Projects/Projects';
 
-class App extends Component {
-  render () {
-    let routes = (
-      <Switch>
-        <Route path="/aboutme" component={AboutMe}/>
-        <Route path="/projects" component={Projects}/>
-        <Route path="/" component={Home}/>
-      </Switch>
-    );
-    return (
-      <div>
-        <Layout>
-          {routes}
-        </Layout>
-      </div>
-    );
-  }
+const App = () => {
+  useEffect(() => {
+    ReactGa.initialize(process.env.GA_TRACKING_ID);
+
+    //report page view
+    ReactGa.pageview(window.location.pathname + window.location.search);
+  }, [])
+
+  let routes = (
+    <Switch>
+      <Route path="/aboutme" component={AboutMe}/>
+      <Route path="/projects" component={Projects}/>
+      <Route path="/" component={Home}/>
+    </Switch>
+  );
+  return (
+    <div>
+      <Layout>
+        {routes}
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Home/HomeSummary/HomeSummary.js
+++ src/components/Home/HomeSummary/HomeSummary.js
@@ -3,6 +3,7 @@ import {FaLink} from 'react-icons/fa';
 import {FaPaperclip} from 'react-icons/fa';
 import {IoIosArrowForward} from 'react-icons/io';
 import { NavLink } from 'react-router-dom';
+import ReactGA from 'react-ga';
 
 import Resume from '../../../assets/files/AliceLuResume.pdf';
 import ResumeIcon from '../../../assets/images/Home/resume-icon.png';
@@ -34,6 +35,15 @@ const homeSummary = () => {
             name: 'Github'
         }
     ]
+
+    //send navlink clicked information to Google Analytics
+    const LearnMoreClickedHandler = () => {
+        ReactGA.event({
+            category: 'NavLink',
+            action: 'Learn more about me clicked from home page'
+        });
+    }
+
     return (
         <div className={classes.HomeSummary}>
             <p className={classes.IntroQuote}>" There has to be a better way to do this! "</p>
@@ -65,6 +75,7 @@ const homeSummary = () => {
             <NavLink 
                 className={classes.LearnMoreText}
                 exact to="/aboutme"
+                onClick={LearnMoreClickedHandler}
             >
                 Learn More
                 <IoIosArrowForward className={classes.RightArrowIcon}/>
