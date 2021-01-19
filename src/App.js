6bebc82ae2e88f9e1657122454c5230479ccd7a6
Alice Lu <alicelu625@gmail.com>
2021-01-18 16:26:41 -0800

    Revert "set up Google Analytics"
    This reverts commit 5709419d0b0712aab69225bd44b07c9184196954.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
-import React, {useEffect} from 'react';
+import React, {Component} from 'react';
 import {Route, Switch} from 'react-router-dom';
-import ReactGa from 'react-ga';
 
 import Layout from './hoc/Layout/Layout';
 import Home from './containers/Home/Home';
 import AboutMe from './containers/AboutMe/AboutMe';
 import Projects from './containers/Projects/Projects';
 
-const App = () => {
-  useEffect(() => {
-    ReactGa.initialize(process.env.GA_TRACKING_ID);
-
-    //report page view
-    ReactGa.pageview(window.location.pathname + window.location.search);
-  }, [])
-
-  let routes = (
-    <Switch>
-      <Route path="/aboutme" component={AboutMe}/>
-      <Route path="/projects" component={Projects}/>
-      <Route path="/" component={Home}/>
-    </Switch>
-  );
-  return (
-    <div>
-      <Layout>
-        {routes}
-      </Layout>
-    </div>
-  );
+class App extends Component {
+  render () {
+    let routes = (
+      <Switch>
+        <Route path="/aboutme" component={AboutMe}/>
+        <Route path="/projects" component={Projects}/>
+        <Route path="/" component={Home}/>
+      </Switch>
+    );
+    return (
+      <div>
+        <Layout>
+          {routes}
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
