abe3126626513429ed324f92165f19837241249b
Alice Lu <alicelu625@gmail.com>
2021-01-18 18:27:41 -0800

    reverted changes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
-import React, {useEffect} from 'react';
+import React, {Component} from 'react';
 import {Route, Switch} from 'react-router-dom';
-import ReactGA from 'react-ga';
 
 import Layout from './hoc/Layout/Layout';
 import Home from './containers/Home/Home';
 import AboutMe from './containers/AboutMe/AboutMe';
 import Projects from './containers/Projects/Projects';
 
-const App = () => {
-  useEffect(() => {
-    ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
-
-    //report page view
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
-
-  let routes = (
-    <Switch>
-      <Route path="/aboutme" component={AboutMe}/>
-      <Route path="/projects" component={Projects}/>
-      <Route path="/" component={Home}/>
-    </Switch>
-  );
-  return (
-    <div>
-      <Layout>
-        {routes}
-      </Layout>
-    </div>
-  );
+class App extends Component {
+  render() {
+    let routes = (
+      <Switch>
+        <Route path="/aboutme" component={AboutMe}/>
+        <Route path="/projects" component={Projects}/>
+        <Route path="/" component={Home}/>
+      </Switch>
+    );
+    return (
+      <div>
+        <Layout>
+          {routes}
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Home/HomeSummary/HomeSummary.js
+++ src/components/Home/HomeSummary/HomeSummary.js
@@ -3,7 +3,6 @@ import {FaLink} from 'react-icons/fa';
 import {FaPaperclip} from 'react-icons/fa';
 import {IoIosArrowForward} from 'react-icons/io';
 import { NavLink } from 'react-router-dom';
-import ReactGA from 'react-ga';
 
 import Resume from '../../../assets/files/AliceLuResume.pdf';
 import ResumeIcon from '../../../assets/images/Home/resume-icon.png';
@@ -36,13 +35,6 @@ const homeSummary = () => {
         }
     ];
 
-    const LearnMoreClickedHandler = () => {
-        ReactGA.event({
-            category: 'NavLink',
-            action: 'Learn More About Me clicked from home page'
-        });
-    }
-
     return (
         <div className={classes.HomeSummary}>
             <p className={classes.IntroQuote}>" There has to be a better way to do this! "</p>
@@ -74,7 +66,6 @@ const homeSummary = () => {
             <NavLink 
                 className={classes.LearnMoreText}
                 exact to="/aboutme"
-                onClick={LearnMoreClickedHandler}
             >
                 Learn More
                 <IoIosArrowForward className={classes.RightArrowIcon}/>
